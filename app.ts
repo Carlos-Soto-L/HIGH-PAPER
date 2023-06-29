83a7aaedd48dddc653be8f471125f8e407f722c6
Carlos Daniel <carlosoto191@gmail.com>
2023-06-29 10:08:05 -0500

    Se implementó la conexión a MongoDB

--- app.ts
+++ app.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import authRouter from './routers/rtr_authentication';
+import { connectToDatabase } from "./services/svc_databaseMongoDB"
 
 const app = express();
 const port = 3000;
+
+// CONEXIÓN A BASE DE DATOS
+connectToDatabase();
+
+// RUTAS
 app.use('/auth',authRouter);
-// app.get('/', (req, res) => {
-//   res.send('HOLA, BIENVENIDO');
-// });
+
 app.listen(port)
   .on('listening', () => {
-    console.log(`Server is listening on ${port}`);
+    console.log(`El servidor está escuchando en el puerto ${port}`);
   })
   .on('error', (err: any) => {
     console.error(err);
